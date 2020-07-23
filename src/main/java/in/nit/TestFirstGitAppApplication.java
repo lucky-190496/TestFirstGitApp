ff61f0017ab997c36902e55af0084a02d3bd8829
lucky-190496 <lucky.gupta1904@gmail.com>
2020-07-23 14:33:39 +0530

    First File Change

---
+++ src/main/java/in/nit/TestFirstGitAppApplication.java
@@ -0,0 +1,13 @@
+package in.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestFirstGitAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestFirstGitAppApplication.class, args);
+	}
+
+}
